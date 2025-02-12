f77e172d6ed56c80b38158f8181cb96ce994e38b
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2025-02-12 16:27:31 +0700

    feat: add migration file

---
+++ TrainingVoteBE/TrainingVoteBE/Migrations/20250212075405_Initial.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TrainingVoteBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Votes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Votes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VoteOptions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    VoteId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VoteOptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VoteOptions_Votes_VoteId",
+                        column: x => x.VoteId,
+                        principalTable: "Votes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Votes",
+                columns: new[] { "Id", "Description", "Title" },
+                values: new object[] { "1", "Choose your favorite programming language", "What is your favorite programming language?" });
+
+            migrationBuilder.InsertData(
+                table: "VoteOptions",
+                columns: new[] { "Id", "Count", "Text", "VoteId" },
+                values: new object[,]
+                {
+                    { "1", 0, "C#", "1" },
+                    { "2", 0, "Java", "1" },
+                    { "3", 0, "Python", "1" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VoteOptions_VoteId",
+                table: "VoteOptions",
+                column: "VoteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VoteOptions");
+
+            migrationBuilder.DropTable(
+                name: "Votes");
+        }
+    }
+}
